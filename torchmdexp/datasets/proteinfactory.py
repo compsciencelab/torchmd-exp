8973d7f6c67d573e3c821040170517c4c0cf70ff
11carlesnavarro <11carlesnavarro@gmail.com>
2022-01-25 08:28:24 +0100

    datasets

---
+++ torchmdexp/datasets/proteinfactory.py
@@ -0,0 +1,42 @@
+from .proteins import ProteinDataset
+import os
+import numpy as np
+
+class ProteinFactory:
+    
+    def __init__(self, datasets, ids, topology=('bonds', 'angles', 'dihedrals')):
+        self.ids = [l.rstrip() for l in open(os.path.join(datasets, ids))]
+        self.topology = topology
+        
+        self.levels = {}
+        self.num_levels = 0
+        
+    def set_proteins_dataset(self, data_dir):
+        
+        proteins = ProteinDataset(data_dir, self.ids, self.topology)
+        return proteins
+    
+    def get_ground_truth(self, level):
+        
+        return self.levels[level]['ground_truth']
+    
+    def set_levels(self, levels_dir):
+        levels = [filename for filename in os.listdir(levels_dir)]
+        
+        self.num_levels = len(levels)
+        
+        [self.set_level(idx, os.path.join(levels_dir, l)) for idx, l in enumerate(levels)]
+    
+    def set_level(self, level, levels_dir):
+        self.levels[level] = {'ground_truth': self.set_proteins_dataset(os.path.join(levels_dir, 'ground_truth')),
+                              'init_states': self.set_proteins_dataset(os.path.join(levels_dir, 'init_states'))
+                             }
+        
+    def get_level(self, level):
+        
+        return self.levels[level]['init_states']
+    
+    def get_num_levels(self):
+        return self.num_levels
+        
+        
