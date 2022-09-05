fbd8fd3dde254cda7fbb7492cce9110acb5fc3db
Eloi Sanchez <eloisanchez16@gmail.com>
2022-09-05 14:15:30 +0200

    Changed conditional so min_val_loss is not set up before 10 epoch levels

--- scripts/train_protein_docking/torchmd/train_dock.py
+++ scripts/train_protein_docking/torchmd/train_dock.py
@@ -177,7 +177,8 @@ def main():
             if val_loss < args.max_val_loss and val_loss < min_val_loss:
                 min_val_loss = val_loss
                 learner.save_model()
-
+                if epoch_level < 10: min_val_loss = args.thresh_lvlup * 1.1:
+                
             if val_loss < 2 * args.thresh_lvlup and (epoch % 5) == 0:
                 lr *= args.lr_decay
                 lr = args.min_lr if lr < args.min_lr else lr
