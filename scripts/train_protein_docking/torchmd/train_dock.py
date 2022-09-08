LevelFactory datasets especially useful for level trainings. Also standarized some names betewen folding and docking. Fixed some problems with the loss calculation without energy term.

scripts/train_protein_docking/torchmd/train_dock.py
@@ -1,6 +1,6 @@
 import argparse
 import torch
-from torchmdexp.datasets.proteinfactory import ProteinFactory
+from torchmdexp.datasets.levelsfactory import LevelsFactory
 from torchmdexp.samplers.torchmd.torchmd_sampler import TorchMD_Sampler
 from torchmdexp.samplers.utils import moleculekit_system_factory
 from torchmdexp.scheme.scheme import Scheme
@@ -8,7 +8,7 @@ from torchmdexp.weighted_ensembles.weighted_ensemble import WeightedEnsemble
 from torchmdexp.learner import Learner
 from torchmdexp.metrics.losses import Losses
 from torchmd.utils import LoadFromFile
-from torchmdexp.metrics.rmsd import rmsd
+from torchmdexp.metrics.ligand_rmsd import ligand_rmsd
 from moleculekit.molecule import Molecule
 from torchmdexp.nnp import models
 from torchmdexp.nnp.models import output_modules
@@ -50,18 +50,8 @@ def main():
     input_file.close()
 
     # Load training molecules
-    train_names = [l.rstrip() for l in open(os.path.join(args.datasets, args.train_set))]
-    protein_factory = ProteinFactory(args.datasets, args.train_set, args.num_levels)
-    protein_factory.set_levels(args.levels_dir)
-    train_ground_truth = protein_factory.get_ground_truth(0)
-    
-    # Load test molecules
-    if args.test_set:
-        test_names = [l.rstrip() for l in open(os.path.join(args.datasets, args.test_set))]
-        test_protein_factory = ProteinFactory(args.datasets, args.test_set)
-        test_protein_factory.set_levels(args.test_dir)
-        test_ground_truth = test_protein_factory.get_ground_truth(0)
-
+    levels_factory = LevelsFactory(args.datasets, args.levels_dir, args.num_levels, out_dir=args.log_dir)
+    train_names = levels_factory.get_names()
     
     # 1. Define the Sampler which performs the simulation and returns the states and energies
     
@@ -79,8 +69,8 @@ def main():
     
     # 2. Define the Weighted Ensemble that computes the ensemble of states   
     loss = Losses(0.0, fn_name='margin_ranking', margin=0.0, y=1.0)
-    weighted_ensemble_factory = WeightedEnsemble.create_factory(nstates = nstates, lr=lr, metric = rmsd, loss_fn=loss,
-                                                                val_fn=rmsd,
+    weighted_ensemble_factory = WeightedEnsemble.create_factory(nstates = nstates, lr=lr, metric = ligand_rmsd, loss_fn=loss,
+                                                                val_fn=ligand_rmsd,
                                                                 max_grad_norm = args.max_grad_norm, T = args.temperature, 
                                                                 replicas = args.replicas, precision = torch.double)
 
@@ -91,7 +81,7 @@ def main():
     # Core
     params.update({'sim_factory': torchmd_sampler_factory,
                    'systems_factory': moleculekit_system_factory,
-                   'systems': train_ground_truth,
+                   'systems': levels_factory.level(levels_factory.num_levels),
                    'nnp': nnp,
                    'device': args.device,
                    'weighted_ensemble_factory': weighted_ensemble_factory,
@@ -121,12 +111,12 @@ def main():
 
     # 4. Define Learner
     learner = Learner(scheme, steps, output_period, train_names=train_names, log_dir=args.log_dir, save_traj=args.save_traj,
-                      keys = ('level', 'steps', 'train_loss', 'val_loss', 'epoch'))    
+                      keys = ('epoch', 'level', 'steps', 'train_loss', 'val_loss', 'loss_1', 'loss_2'))    
 
     
     # 5. Define epoch and Levels
     epoch = 0
-    num_levels = protein_factory.get_num_levels()
+    num_levels = levels_factory.num_levels
     
     init_state = None
     
@@ -134,14 +124,14 @@ def main():
     for level in range(num_levels):
         
         assert args.max_val_loss > args.thresh_lvlup
-        min_val_loss = args.max_val_loss
+        min_train_loss = args.max_val_loss
         lvl_up = False
         lr_warn = True
         epoch_level = 0
         
         # Update level
-        ground_truth = protein_factory.get_ground_truth(level=0)  # Ground state is always docked state
-        init_states = protein_factory.get_level(level, from_gt=True)  # Get levels from ground truths
+        train_set = levels_factory.level(level)
+        print(f"in level {level}, using: {train_set.get('names')}")
             
         # Set sim batch size:
         while sim_batch_size > args.sim_batch_size:
@@ -149,37 +139,46 @@ def main():
             
         while not lvl_up:
             
-            ground_truth = ground_truth[:]
-            init_states = init_states[:]
-            # random.shuffle(ground_truth) # rdmize systems TODO: Implement it along with init_states
-            
-            for i in range(0, len(ground_truth), sim_batch_size):
-                batch_ground_truth = ground_truth[i:i+sim_batch_size]
-                batch_init_states = init_states[i:i+sim_batch_size]
-                learner.set_ground_truth(batch_ground_truth)
-                learner.set_init_state(batch_init_states)
-                learner.step()
-
-            # Compute test loss
             epoch += 1
             epoch_level += 1
-            if args.test_set:
-                if (epoch == 1 or (epoch % args.test_freq) == 0):
-                    learner.set_ground_truth(test_ground_truth)
-                    learner.step(test=True)
+            
+            train_set.shuffle() # rdmize systems
+            
+            print(f"level {level}, using: {train_set.get('names')}")
+            print(f"epoch {epoch}  epoch_level {epoch_level}.")
+            for i in range(0, len(train_set.get('names')), sim_batch_size):
+                # Get batch
+                batch = train_set[i:sim_batch_size+i]
+                learner.set_batch(batch)
+                print(f"Current batch {batch.get('names')}")
+                learner.step()
+
+            # Val step
+            #if len(val_set) > 0:
+            #    val_set.shuffle()
+            #    if (epoch == 1 or (epoch % args.val_freq) == 0):
+            #        for i in range(0, val_set_size, sim_batch_size):
+            #            batch = val_set[ i : sim_batch_size + i]
+            #            learner.set_batch(batch)
+            #            learner.step(val=True)
+            
+            #if args.test_set:
+            #    if (epoch == 1 or (epoch % args.test_freq) == 0):
+            #        learner.set_ground_truth(test_ground_truth)
+            #        learner.step(test=True)
 
             # Get training process information
             learner.compute_epoch_stats()
             learner.write_row()
-            val_loss = learner.get_val_loss()
+            train_loss = learner.get_train_loss()
 
             # Save
-            if val_loss < args.max_val_loss and val_loss < min_val_loss:
-                min_val_loss = val_loss
+            if train_loss < args.max_val_loss and train_loss < min_train_loss:
+                min_train_loss = train_loss
                 learner.save_model()
-                if epoch_level < 10: min_val_loss = args.thresh_lvlup * 1.1
+                if epoch_level < 10: min_train_loss = args.thresh_lvlup * 1.1
                 
-            if val_loss < 2 * args.thresh_lvlup and (epoch % 5) == 0:
+            if train_loss < 2 * args.thresh_lvlup and (epoch % 5) == 0:
                 lr *= args.lr_decay
                 lr = args.min_lr if lr < args.min_lr else lr
                 if lr == args.min_lr and lr_warn: 
@@ -196,7 +195,9 @@ def main():
             
             # Check before level up. If last level -> Don't level up. Spend at least 10 epochs per level
             if min_val_loss < args.thresh_lvlup and level + 1 < args.num_levels and epoch_level >= 10:
-                print(f'Leveling up to level {level+1} with validation loss: {min_val_loss} < {args.thresh_lvlup}')
+                
+                print(f'Leveling up to level {level+1} with training loss: {min_train_loss:.2f} < {args.thresh_lvlup}')
+                
                 lvl_up = True
                 learner.level_up()
                 

torchmdexp/samplers/openmm/openmm_sampler.py
@@ -22,7 +22,7 @@ class OpenMM_Sampler(Sampler):
         Contain the system to simulate. Can have more than one molecule
     nnp: LightningModule
         Neural Network Potential used to simulate the system
-    mls: list
+    lengths: list
         Contains the lengths of each molecule in the Moleculekit object
     focefield: str
         Directory of the forcefield file
@@ -56,7 +56,7 @@ class OpenMM_Sampler(Sampler):
     ------------
     precision: torch.precision
         'Floating point precision'
-    mls: list
+    lengths: list
         Contains the lengths of each molecule in the Moleculekit object
     sim_dict: dict
         Dict containing information about each state (coordinates) and prior Energy of each molecule simulated
@@ -69,9 +69,9 @@ class OpenMM_Sampler(Sampler):
                  mols,
                  nnp,
                  device,
-                 mls,
+                 lengths,
                  names,
-                 ground_truth,
+                 ground_truths,
                  forcefield, 
                  forceterms,
                  cutoff, 
@@ -80,7 +80,7 @@ class OpenMM_Sampler(Sampler):
                  langevin_gamma=0.1 
                 ):
         
-        self.mls = mls
+        self.lengths = lengths
         self.names = names
         self.device = device
         self.forcefield = forcefield
@@ -117,7 +117,7 @@ class OpenMM_Sampler(Sampler):
         
 
         # ------------------- Set the ground truth list (PDB coordinates) -----------
-        self.ground_truth = {name: ground_truth[idx] for idx, name in enumerate(names)}
+        self.ground_truths = {name: ground_truths[idx] for idx, name in enumerate(names)}
         self.init_coords = None
         
         # Create the dictionary used to return states and prior energies
@@ -171,13 +171,13 @@ class OpenMM_Sampler(Sampler):
             creates a new TorchMD_Sampler instance.
         """
 
-        def create_sampler_instance(mol, nnp, device, mls, names, ground_truth):
+        def create_sampler_instance(mol, nnp, device, lengths, names, ground_truths):
             return cls(mol,
                        nnp,
                        device,
-                       mls, # molecule lengths
+                       lengths, # molecule lengths
                        names,
-                       ground_truth,
+                       ground_truths,
                        forcefield, 
                        forceterms,
                        cutoff, 
@@ -211,13 +211,13 @@ class OpenMM_Sampler(Sampler):
         self.nnp.load_state_dict(weights)
         self.nnp_op = self._wrap_nnp(self.nnp, self.elements)
     
-    def set_ground_truth(self, ground_truth):
+    def set_ground_truth(self, ground_truths):
         
-        self.names = [mol.viewname[:-4] for mol in ground_truth]
-        self.mls = [len(mol.resname) for mol in ground_truth]
-        gt_dict = {name: {'ground_truth': get_native_coords(ground_truth[idx])} for idx, name in enumerate(self.names)}
+        self.names = [mol.viewname[:-4] for mol in ground_truths]
+        self.lengths = [len(mol.resname) for mol in ground_truths]
+        gt_dict = {name: {'ground_truths': get_native_coords(ground_truths[idx])} for idx, name in enumerate(self.names)}
         self.sim_dict = gt_dict
-        self.mol = create_system(ground_truth)
+        self.mol = create_system(ground_truths)
     
     def set_init_state(self, init_states):
         """

torchmdexp/samplers/torchmd/torchmd_sampler.py
@@ -24,7 +24,7 @@ class TorchMD_Sampler(Sampler):
         Contain the system to simulate. Can have more than one molecule
     nnp: LightningModule
         Neural Network Potential used to simulate the system
-    mls: list
+    lengths: list
         Contains the lengths of each molecule in the Moleculekit object
     focefield: str
         Directory of the forcefield file
@@ -58,7 +58,7 @@ class TorchMD_Sampler(Sampler):
     ------------
     precision: torch.precision
         'Floating point precision'
-    mls: list
+    lengths: list
         Contains the lengths of each molecule in the Moleculekit object
     sim_dict: dict
         Dict containing information about each state (coordinates) and prior Energy of each molecule simulated
@@ -71,9 +71,9 @@ class TorchMD_Sampler(Sampler):
                  mols,
                  nnp,
                  device,
-                 mls,
+                 lengths,
                  names,
-                 ground_truth,
+                 ground_truths,
                  forcefield, 
                  forceterms,
                  replicas, 
@@ -86,6 +86,7 @@ class TorchMD_Sampler(Sampler):
                  temperature=350,
                  langevin_temperature=350,
                  langevin_gamma=0.1,
+                 init_states = None,
                  x = None,
                  y = None,
                  ff_type='file',
@@ -95,7 +96,7 @@ class TorchMD_Sampler(Sampler):
                 ):
         
         self.mols = mols
-        self.mls = mls
+        self.lengths = lengths
         self.names = names
         self.x = x
         self.y = y
@@ -123,8 +124,9 @@ class TorchMD_Sampler(Sampler):
         self.nnp = nnp
 
         # ------------------- Set the ground truth list (PDB coordinates) -----------
-        self.ground_truth = {name: ground_truth[idx] for idx, name in enumerate(names)}
-        self.init_coords = None
+        self.ground_truths = {name: ground_truths[idx] for idx, name in enumerate(names)}
+        
+        self.init_coords = None  # Set up in set_init_state()
         
         # Create the dictionary used to return states and prior energies
         self.sim_dict = collections.defaultdict(dict)
@@ -187,13 +189,13 @@ class TorchMD_Sampler(Sampler):
             creates a new TorchMD_Sampler instance.
         """
 
-        def create_sampler_instance(mol, nnp, device, mls, names, ground_truth, x=None, y=None):
+        def create_sampler_instance(mol, nnp, device, lengths, names, ground_truths, init_states=None, x=None, y=None):
             return cls(mol,
                        nnp,
                        device,
-                       mls, # molecule lengths
+                       lengths, # molecule lengths
                        names,
-                       ground_truth,
+                       ground_truths,
                        forcefield, 
                        forceterms,
                        replicas, 
@@ -206,6 +208,7 @@ class TorchMD_Sampler(Sampler):
                        temperature,
                        langevin_temperature,
                        langevin_gamma,
+                       init_states,
                        x,
                        y,
                        ff_type,
@@ -237,7 +240,7 @@ class TorchMD_Sampler(Sampler):
             
         # Iterator and start computing forces
         iterator = range(1,int(steps/output_period)+1)
-        integrator = self._set_integrator(self.mols, self.mls)
+        integrator = self._set_integrator(self.mols, self.lengths)
         
         # Define the states
         nstates = int(steps // output_period)
@@ -285,21 +288,26 @@ class TorchMD_Sampler(Sampler):
     def set_batch(self, batch):
         
         self.names = batch.get('names')
-        self.mls = batch.get('lengths')
+        self.lengths = batch.get('lengths')
         self.mols = batch.get('molecules')
         
+        if 'init_states' in batch.get_keys():
+            for mol, init_state in zip(self.mols, batch.get('init_states')):
+                mol.coords = init_state[:, :, None]
+            self.init_coords = self.set_init_state(self.mols)
+        
         self.sim_dict['names'] = self.names
-        self.sim_dict['ground_truth'] = batch.get('observables')
+        self.sim_dict['ground_truths'] = batch.get('ground_truths')
         self.sim_dict['mols'] = self.mols
         
-    def _set_integrator(self, mols, mls):
+    def _set_integrator(self, mols, lengths):
         
         # Create simulation system
         mol = create_system(mols)
         
         if self.init_coords is not None:
             mol.coords = self.init_coords
-                        
+        
         # Create embeddings and the external force
         embeddings = get_embeddings(mol, self.device, self.replicas)
         external = External(self.nnp, embeddings, device = self.device)
@@ -307,7 +315,7 @@ class TorchMD_Sampler(Sampler):
         # Add the embeddings to the sim_dict
         my_e = embeddings 
         self.sim_dict['embeddings'] = []
-        for idx, ml in enumerate(mls):
+        for idx, ml in enumerate(lengths):
             mol_embeddings, my_e = my_e[:, :ml], my_e[:, ml:]
             self.sim_dict['embeddings'].append(mol_embeddings.to('cpu'))     
 
@@ -341,7 +349,7 @@ class TorchMD_Sampler(Sampler):
         """
         Split the states tensor and adds the coordinates of each molecule to the sample_dict
         """
-        for idx, ml in enumerate(self.mls):
+        for idx, ml in enumerate(self.lengths):
             states_mol, states = states[:, :ml, :], states[:, ml:, :]
             sample_dict['states'].append(states_mol)
         return sample_dict

torchmdexp/weighted_ensembles/weighted_ensemble.py
@@ -138,7 +138,7 @@ class WeightedEnsemble:
         
         return neff
     
-    def compute_we(self, states, mols, ground_truth, embeddings, U_prior, nnp_prime, neff_threshold=None):
+    def compute_we(self, states, mols, ground_truths, embeddings, U_prior, nnp_prime, neff_threshold=None):
         
         weights, U_ext_hat = self._weights(states, embeddings, U_prior, nnp_prime)
 
@@ -147,29 +147,30 @@ class WeightedEnsemble:
         # Compute the weighted ensemble of the conformations 
         states = states.to(self.device)
         
-        obs = torch.tensor([self.metric(state, ground_truth, mols) for state in states], device = self.device, dtype = self.precision)
+        obs = torch.tensor([self.metric(state, ground_truths, mols) for state in states], device = self.device, dtype = self.precision)
         avg_metric = torch.mean(obs).detach().item()
 
         w_ensemble = torch.multiply(weights, obs).sum(0) 
         
         return w_ensemble, avg_metric
     
-    def compute_loss(self, ground_truth, mols, states, embeddings, U_prior, nnp_prime, x = None, y = None, energy_weight=1.0):
+    def compute_loss(self, ground_truths, mols, states, embeddings, U_prior, nnp_prime, x = None, y = None, energy_weight=0):
         
-        w_e, avg_metric = self.compute_we(states, mols, ground_truth, embeddings, U_prior, nnp_prime)
+        w_e, avg_metric = self.compute_we(states, mols, ground_truths, embeddings, U_prior, nnp_prime)
         values_dict = {}
+        we_loss = self.loss_fn(w_e)
+        
         if energy_weight == 0:
-            loss = self.loss_fn(w_e)
-            
+            loss = we_loss
+            values_dict['loss_2'] = None
         else:
-            we_loss = self.loss_fn(w_e)
             energy_loss = self.compute_energy_loss(x, y, embeddings, nnp_prime)
             
             loss = we_loss + energy_weight * energy_loss
             values_dict['loss_2'] = energy_loss.item()
         
         values_dict['avg_metric'] = avg_metric
-        values_dict['loss_1'] = we_loss.item()        
+        values_dict['loss_1'] = loss.item()
         
         return loss, values_dict
         
@@ -200,11 +201,11 @@ class WeightedEnsemble:
         return l1_loss(y, forces)
         
     
-    def compute_gradients(self, names, mols, ground_truth, states, embeddings, U_prior, nnp_prime, x = None, y = None, grads_to_cpu=True, val=False):
+    def compute_gradients(self, names, mols, ground_truths, states, embeddings, U_prior, nnp_prime, x = None, y = None, grads_to_cpu=True, val=False):
         
         if val == False:
             self.optimizer.zero_grad()
-            loss, values_dict = self.compute_loss(ground_truth, mols, states, embeddings, U_prior, nnp_prime, x = x, y = y)
+            loss, values_dict = self.compute_loss(ground_truths, mols, states, embeddings, U_prior, nnp_prime, x = x, y = y)
             loss.backward()
             torch.nn.utils.clip_grad_norm_(self.nnp.parameters(), self.max_grad_norm)
 
@@ -219,7 +220,7 @@ class WeightedEnsemble:
                         grads.append(p.grad)
         elif val == True:
             with torch.no_grad():
-                loss = self.compute_loss(ground_truth, mols, states, embeddings, U_prior, nnp_prime)
+                loss = self.compute_loss(ground_truths, mols, states, embeddings, U_prior, nnp_prime)
                     
         return grads, loss.item(), values_dict
         
@@ -227,17 +228,17 @@ class WeightedEnsemble:
     def get_loss(self):
         return self.loss.detach().item()
     
-    def compute_val_loss(self, ground_truth, states, **kwargs):
+    def compute_val_loss(self, ground_truths, states, **kwargs):
         
         # Compute val loss
         
         n_states = 'last'
         if n_states == 'last':
-            val_rmsd = self.val_fn(states[-1], ground_truth).item()
+            val_rmsd = self.val_fn(states[-1], ground_truths).item()
         elif n_states == 'last10':
-            val_rmsd = mean([self.val_fn(ground_truth, state).item() for state in states[-10:]])
+            val_rmsd = mean([self.val_fn(ground_truths, state).item() for state in states[-10:]])
         else:
-            val_rmsd = mean([self.val_fn(ground_truth, state).item() for state in states])   
+            val_rmsd = mean([self.val_fn(ground_truths, state).item() for state in states])   
         
         #self.init_coords = states[-1]
         
@@ -256,9 +257,9 @@ class WeightedEnsemble:
         for g in self.optimizer.param_groups:
             g['lr'] = lr
     
-    def get_native_U(self, ground_truth, embeddings):
-        ground_truth = ground_truth.unsqueeze(0)
-        return self._extEpot(ground_truth, embeddings, mode='val')
+    def get_native_U(self, ground_truths, embeddings):
+        ground_truths = ground_truths.unsqueeze(0)
+        return self._extEpot(ground_truths, embeddings, mode='val')
     
     def get_init_state(self):
         return self.init_coords