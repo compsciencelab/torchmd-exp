a9f6ad97f2d895b54ffabb01de227a848e56f240
11carlesnavarro <11carlesnavarro@gmail.com>
2022-10-03 18:34:47 +0200

    test loss

--- torchmdexp/learner.py
+++ torchmdexp/learner.py
@@ -16,9 +16,10 @@ class Learner:
         Directory for model checkpoints and the monitor.csv
     """
     
-    def __init__(self, scheme, steps, output_period, train_names = [] , log_dir=None, keys=('train_loss', 'val_loss')):
+    def __init__(self, scheme, steps, output_period, train_names = [] , log_dir=None, keys=('epoch', 'train_loss', 'val_loss')):
         self.log_dir = log_dir
         self.update_worker = scheme.update_worker()
+        self.keys = keys
         
         # Counters and metrics
         self.steps = steps
@@ -26,59 +27,65 @@ class Learner:
         self.log_dir = log_dir
         self.train_names = train_names
         
+        # Train losses of each batch
         self.train_losses = []
         self.val_losses = []
-
+        self.test_losses = []
+        
+        # Individual Losses of the epoch
         self.loss_1 = []
         self.loss_2 = []
         self.val_loss_1 = []
         self.val_loss_2 = []
-
+        
+        # Losses of the epoch
         self.train_loss = None
         self.val_loss = None
+        self.test_loss = None
+        
+        # Level, epoch and LR
         self.level = 0
-        self.epoch = 1
+        self.epoch = 0
         self.lr = None
         
         # Prepare results dict
-        self.results_dict = {'epoch': self.epoch, 'level':self.level, 'steps': self.steps, 'train_loss': None, 'val_loss': None}
-        total_dict = {}
-        for key in keys:
-            if key not in total_dict.keys():
-                total_dict[key] = 0
-                
-        for name in self.train_names:
-            total_dict[name] = None
+        self.results_dict = {key: 0 for key in keys}
+        if len(self.train_names) > 0:
+            for name in self.train_names:
+                self.results_dict[name] = None
         
-        self.results_dict.update(total_dict)
+        #self.results_dict.update(total_dict)
+        
         keys = tuple([key for key in self.results_dict.keys()])
         self.logger = LogWriter(self.log_dir,keys=keys)
 
-    def step(self, val=False):
+    def step(self, val=False, mode='val'):
         """ Takes an optimization update step """
         
         # Update step
         info = self.update_worker.step(self.steps, self.output_period, val)
         
-        self.results_dict['level'] = self.level
-        self.results_dict['steps'] = self.steps
+        if 'level' in self.keys:
+            self.results_dict['level'] = self.level
+        if 'steps' in self.keys:
+            self.results_dict['steps'] = self.steps
         
-        if val == False:
-            self.train_losses.append(info['train_loss'])
-            if 'loss_1' in self.results_dict.keys():
-                self.loss_1.append(info['loss_1'])
-            if 'loss_2' in self.results_dict.keys():
-                self.loss_2.append(info['loss_1'])
-        else:
-            self.val_losses.append(info['val_loss'])
-            if 'val_loss_1' in self.results_dict.keys():
+        if val == True:
+            if mode == 'val':
+                self.val_losses.append(info['val_loss'])
                 self.val_loss_1.append(info['val_loss_1'])
-            if 'val_loss_2' in self.results_dict.keys():
-                self.val_loss_2.append(info['val_loss_1'])
+                self.val_loss_2.append(info['val_loss_2'])
+            elif mode == 'test':
+                self.test_losses.append(info['val_loss'])
+        else:
+            self.train_losses.append(info['train_loss'])
+            self.loss_1.append(info['loss_1'])
+            self.loss_2.append(info['loss_2'])
         
-        [info.pop(k, None) for k in ['train_loss', 'val_loss', 'loss_1', 'loss_2', 'val_loss_1', 'val_loss_2']]
-
-        self.results_dict.update(info)
+        [info.pop(k, None) for k in ['train_loss', 'val_loss', 'test_loss', 'loss_1', 'loss_2', 'val_loss_1', 'val_loss_2']]
+        
+        if len(self.train_names) > 0:
+            self.results_dict.update(info)
 
     def level_up(self):
         """ Increases level of difficulty """
@@ -117,32 +124,50 @@ class Learner:
     def compute_epoch_stats(self):
         """ Compute epoch val loss and train loss averages and update epoch number"""
         
+        # Compute train loss
         self.train_loss = mean(self.train_losses)
         self.results_dict['train_loss'] = self.train_loss
+        
+        # Update epoch
+        self.epoch += 1
         self.results_dict['epoch'] = self.epoch
 
-        if len(self.val_losses) > 0:
-            self.val_loss = mean(self.val_losses)
-            self.results_dict['val_loss'] = self.val_loss
-            self.val_losses = []
-        else:
-            self.results_dict['val_loss'] = None
+        
+        # Compute val loss
+        if 'val_loss' in self.keys:
+            if len(self.val_losses) > 0:
+                self.val_loss = mean(self.val_losses)
+                self.results_dict['val_loss'] = self.val_loss
+            else:
+                self.results_dict['val_loss'] = None
+        
+        # Compute test loss
+        if 'test_loss' in self.keys:
+            if len(self.test_losses) > 0:
+                self.test_loss = mean(self.test_losses)
+                self.results_dict['test_loss'] = self.test_loss
+            else:
+                self.results_dict['test_loss'] = None
 
-        self.train_losses = []
-        self.epoch += 1
-
-        if len(self.loss_1) > 0:
+        
+        # Compute loss 1 and 2
+        if 'loss_1' in self.keys:
             self.results_dict['loss_1'] = mean(self.loss_1)
-            self.loss_1 = []
-        if len(self.loss_2) > 0:
-            self.results_dict['loss_1'] = mean(self.loss_2)
-            self.loss_2 = []
-        if len(self.val_loss_1) > 0:
+        if 'loss_2' in self.keys:
+            self.results_dict['loss_2'] = mean(self.loss_2)
+        if 'val_loss_1' in self.keys:
             self.results_dict['val_loss_1'] = mean(self.val_loss_1)
-            self.val_loss_1 = []
-        if len(self.val_loss_1) > 0:
+        if 'val_loss_2' in self.keys:
             self.results_dict['val_loss_2'] = mean(self.val_loss_2)
-            self.val_loss_2 = []
+        
+        # Reset everything
+        self.train_losses = []
+        self.val_losses = []
+        self.test_losses = []
+        self.loss_1 = []
+        self.loss_2 = []
+        self.val_loss_1 = []
+        self.val_loss_2 = []
         
     def write_row(self):
         if self.logger:
